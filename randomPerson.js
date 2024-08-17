98969d1a4be9c349459908a7a30c2ced1a6963d0
Olena Reukova <kronverk01@gmail.com>
2024-08-17 11:17:44 +0100

    random

---
+++ randomPerson.js
@@ -0,0 +1,6 @@
+function whosPlaying(names) {
+  let numberOfPeople = names.length;
+  let randomPersonPosition = Math.floor(Math.random() * numberOfPeople);
+  let randomPerson = names[randomPersonPosition];
+  return randomPerson + ' is going to buy lunch today';
+}
